dfffb2199e888ac6deae0e5b99ba3a0251cfa999
Cristina Dias <33206445+crisgit@users.noreply.github.com>
2023-12-01 23:58:49 -0300

    funcoes recebendo objetos

--- 05 - objetos/classePessoa.js
+++ 05 - objetos/classePessoa.js
@@ -14,11 +14,23 @@ class Pessoa {
     }
 };
 
-const cristina = new Pessoa('Cristina', 1);
-const fulano = new Pessoa('Fulano', 2);
+function compararPessoas(p1, p2) {
+    if (p1.id < p2.id) {
+        console.log(`${p1.nome} tem o registro de cadastro inicial na fila que ${p2.nome}`);
+    } else if (p2.id < p1.id) {
+        console.log(`${p2.nome} tem o registro de cadastro inicial na fila que ${p1.nome}`);
+    } else {
+        console.log(`${p1.nome} e ${p2.nome} tem a mesma posição de cadastro na fila de registro`);
+    } 
+}
 
-console.log(cristina);
-console.log(fulano);
+const cristina = new Pessoa('Cristina', 0);
+const fulano = new Pessoa('Fulano', 1);
+
+compararPessoas(cristina, fulano);
+
+/* console.log(cristina);
+console.log(fulano); */
 
 /* cristina.descrever();
 fulano.descrever(); */
