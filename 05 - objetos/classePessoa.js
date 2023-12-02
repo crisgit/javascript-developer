bf37162c3f940e5ea127db381a6d29415062f85f
Cristina Dias <33206445+crisgit@users.noreply.github.com>
2023-12-01 23:36:25 -0300

    criacao de instancias com o constructor

--- 05 - objetos/classePessoa.js
+++ 05 - objetos/classePessoa.js
@@ -1,24 +1,24 @@
 class Pessoa {
-    // classe é a definição deste objeto dentro de Pessoan
     nome;
     id;
+    registroContadorId;
 
+    // acontece quando uma pessoa é instanciada
+    constructor(nome, id) {
+        this.nome = nome;
+        this.id = id;
+        this.registroContadorId = id - 1;
+    }
     descrever () {
         console.log(`Meu nome é ${this.nome} e meu id é ${this.id}`);
     }
 };
 
-// instância é a ocorrência deste resultado abaixo:
-const cristina = new Pessoa();
-cristina.nome = 'Cristina Dias';
-cristina.id = 1;
+const cristina = new Pessoa('Cristina', 1);
+const fulano = new Pessoa('Fulano', 2);
 
-const fulano = new Pessoa();
-fulano.nome = 'Fulano de Tal';
-fulano.id = 2;
+console.log(cristina);
+console.log(fulano);
 
-// console.log(cristina);
-// console.log(fulano);
-
-cristina.descrever();
-fulano.descrever();
+/* cristina.descrever();
+fulano.descrever(); */
