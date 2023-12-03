fd749edcb94733fd7bcafa53d43557e09057368e
Cristina Dias <33206445+crisgit@users.noreply.github.com>
2023-12-03 20:09:40 -0300

    exercicio maior numero sorteado

--- 07 - importação/main.js
+++ 07 - importação/main.js
@@ -1,3 +1,36 @@
 const { gets, print } = require('./funcoes_auxiliares');
 
-print(gets());
+/* 
+Uma sala contem 5 alunos e para cada aluno foi sorteado um número de 1 - 100. 
+Faça um programa que receba 5 números sorteados para os alunos e mostre o maior número sorteado.
+
+Dados de entrada:
+5
+50
+10
+98
+23
+
+Saída: 
+98
+*/
+
+const numerosSorteados = [];
+
+for (let i = 0; i < 5; i++) {
+    const numeroSorteio = gets();
+    numerosSorteados.push(numeroSorteio);
+}
+
+// print(numeroSorteados);
+
+let maiorValor = 0;
+
+for (let i = 0; i < numerosSorteados.length; i++) {
+    const numeroSorteio = numerosSorteados[i];
+    if (numeroSorteio > maiorValor) {
+        maiorValor = numeroSorteio;
+    }
+}
+
+print(maiorValor);
