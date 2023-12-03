3b934c9042128af13f5c8b6c0dae6b380d5c2b32
Cristina Dias <33206445+crisgit@users.noreply.github.com>
2023-12-03 17:51:33 -0300

    exercicio lista numeros pares

---
+++ 06 - listas (arrays)/pares.js
@@ -0,0 +1,14 @@
+/* 
+2 - Crie um programa que seja capaz de pertencer uma lista de números e imprima cada número par encontrado.
+
+*/
+
+const numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 0; i < numeros.length; i++) {
+    const pares = numeros[i];
+    
+    if (pares % 2 === 0) {
+        console.log(pares);
+    }
+}
