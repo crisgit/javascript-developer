3af8297a503c8aa0b892f4f78d9bf0ff30ca6da8
Cristina Dias <33206445+crisgit@users.noreply.github.com>
2023-12-05 14:00:16 -0300

    adicionando o recurso de fetch api do pokemon

---
+++ pokedex/assets/js/main.js
@@ -0,0 +1,14 @@
+const offset = 0;
+const limit = 10;
+
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => console.log(jsonBody))
+    .catch((error) => console.error(error))
+    .finally(function () {
+        console.log('Resolução concluída!');
+    })
+
+/* console.log(limit); */
