18997dba6bb75af1fdbd4883dd3668fe93696ed0
Cristina Dias <33206445+crisgit@users.noreply.github.com>
2023-12-06 15:58:02 -0300

    adicionado tipos de pokemon dinamicamente, botao e paginacao

--- pokedex/assets/js/poke-api.js
+++ pokedex/assets/js/poke-api.js
@@ -1,8 +1,25 @@
 const pokeApi = {}
 
+function convertPokeApiDetailToPokemon(pokeDetail) {
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDetail.id
+    pokemon.name = pokeDetail.name
+
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+    return pokemon
+}
+
 pokeApi.getPokemonDetail = (pokemon) => {
     return fetch(pokemon.url)
         .then((response) => response.json())
+        .then(convertPokeApiDetailToPokemon)
 }
 
 pokeApi.getPokemons = (offset = 0, limit = 5) => {
@@ -11,12 +28,12 @@ pokeApi.getPokemons = (offset = 0, limit = 5) => {
     return fetch(url)
         .then((response) => response.json())
         .then((jsonBody) => jsonBody.results)
-        .then((pokemons) => pokemons.map((pokeApi.getPokemonDetail)))
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
         .then((detailRequests) => Promise.all(detailRequests))
         .then((pokemonsDetails) => pokemonsDetails)
-        .catch((error) => console.error(error))
-        .finally(function () {
-        console.log('Resolução concluída!');
-    })
+    //     .catch((error) => console.error(error))
+    //     .finally(function () {
+    //     console.log('Resolução concluída!');
+    // })
 }
 
