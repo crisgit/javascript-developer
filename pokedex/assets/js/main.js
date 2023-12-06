00743581e1863cf23f302f4fb05e842d2da84ce9
Cristina Dias <33206445+crisgit@users.noreply.github.com>
2023-12-06 16:28:56 -0300

    recurso de limitar na primeira geracao de pokemon a paginacao

--- pokedex/assets/js/main.js
+++ pokedex/assets/js/main.js
@@ -1,6 +1,8 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
-const limit = 5;
+
+const maxRecords = 151;
+const limit = 10;
 let offset = 0;
 
 function loadPokemonItens(offset, limit) {
@@ -29,5 +31,19 @@ loadPokemonItens(offset, limit)
 
 loadMoreButton.addEventListener('click', () => {
     offset += limit
-    loadPokemonItens(offset, limit)
+
+    debugger
+
+    const qtdRecordPage = offset + limit
+
+    if (qtdRecordPage >= maxRecords) {
+        const newLimit = maxRecords - offset
+        loadPokemonItens(offset, newLimit)
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    } else {
+        loadPokemonItens(offset, limit)
+    }
+
+
 })
