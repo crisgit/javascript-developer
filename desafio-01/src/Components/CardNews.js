3887f563cbc78ad13842f93cf9f5b3021b56b36e
Cristina Dias <33206445+crisgit@users.noreply.github.com>
2023-12-07 23:12:09 -0300

    construcao do build no componente

--- desafio-01/src/Components/CardNews.js
+++ desafio-01/src/Components/CardNews.js
@@ -3,8 +3,37 @@ class CardNews extends HTMLElement {
         super();
 
         const shadow = this.attachShadow({ mode: "open" });
-        shadow.innerHTML = "<h1>Hello Cristina</h1>";
+        shadow.appendChild(this.build());
+        shadow.appendChild(this.styles());
     }
+
+    build() {
+        const componentRoot = document.createElement("div");
+        componentRoot.setAttribute("class", "card");
+
+        const cardLeft = document.createElement("div");
+        cardLeft.setAttribute("class", "card__left");
+
+        const autor = document.createElement("span");
+        const linkTitle = document.createElement("a");
+        const newsContent = document.createElement("p");
+
+        cardLeft.appendChild(autor);
+        cardLeft.appendChild(linkTitle);
+        cardLeft.appendChild(newsContent);
+
+        const cardRight = document.createElement("div");
+        cardRight.setAttribute("class", "card__right");
+        const newsImage = document.createElement("img");
+        cardRight.appendChild(newsImage);
+
+        componentRoot.appendChild(cardLeft);
+        componentRoot.appendChild(cardRight);
+
+        return componentRoot;
+    }
+
+    styles() {}
 }
 
 customElements.define('card-news', CardNews);
