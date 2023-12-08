10effff73e7c8b955136e2425db309966caa1236
Cristina Dias <33206445+crisgit@users.noreply.github.com>
2023-12-08 10:16:27 -0300

    componentes e a estilizacao dos css

--- desafio-01/src/Components/CardNews.js
+++ desafio-01/src/Components/CardNews.js
@@ -33,7 +33,7 @@ class CardNews extends HTMLElement {
 
         const newsImage = document.createElement("img");
         newsImage.src = this.getAttribute("photo") || "assets/images.png";
-        newsImage.alt = "caneca com imagem do darth vader e com um filhote de gato dentro da caneca"
+        newsImage.alt = "caneca com imagem do darth vader e com um filhote de gato dentro da caneca";
         cardRight.appendChild(newsImage);
 
         componentRoot.appendChild(cardLeft);
@@ -42,7 +42,45 @@ class CardNews extends HTMLElement {
         return componentRoot;
     }
 
-    styles() {}
+    styles() {
+        const style = document.createElement("style");
+        style.textContent = `
+            .card {
+                width: 80vw;
+                box-shadow: 10px 13px 32px 0px rgba(0,0,0,0.5);
+                -webkit-box-shadow: 10px 13px 32px 0px rgba(0,0,0,0.5);
+                -moz-box-shadow: 10px 13px 32px 0px rgba(0,0,0,0.5);
+                display: flex;
+                flex-direction: row;
+                justify-content: space-between;
+            }
+            
+            .card__left{
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                padding-left: 0.625rem;
+            }
+            
+            .card__left > span {
+                font-weight: 400;
+            }
+            
+            .card__left > a {
+                margin-top: 0.938rem;
+                font-size: 1.563rem;
+                color: black;
+                text-decoration: none;
+                font-weight: bold;
+            }
+            
+            .card__left > p {
+                color: rgb(70, 70, 70);
+            }
+        `;
+
+        return style;
+    }
 }
 
 customElements.define('card-news', CardNews);
